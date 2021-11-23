1cc5d04c9c8e44fecef907d36bdf5f070f16a171
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2021-11-23 23:10:56 +0530

    implemented tls

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"ftpserver/server/credentials"
+	"crypto/tls"
 	"bufio"
 	"log"
 	"net"
@@ -23,9 +24,41 @@ func init(){
 
 }
 
+const serverKey = `-----BEGIN EC PARAMETERS-----
+BggqhkjOPQMBBw==
+-----END EC PARAMETERS-----
+-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIBGcLjOMtmcOtWGTN/VYLFULNEDxXa3T5YI73q72HUXjoAoGCCqGSM49
+AwEHoUQDQgAEAxgzx96lQzbvC+S2lNkGzFORalrFJ0jC5GWcKoSOyPOnaCRIlUwT
+2lZ9IMDkicWeHpAlPLqm0UxSgPFhEtLTlw==
+-----END EC PRIVATE KEY-----
+`
 
+const serverCert = `-----BEGIN CERTIFICATE-----
+MIICGTCCAb+gAwIBAgIUGqDt34TV7GOlqIxUJeNcBJZuGFYwCgYIKoZIzj0EAwIw
+YjELMAkGA1UEBhMCSU4xDjAMBgNVBAgMBURlbGhpMQ4wDAYDVQQHDAVEZWxoaTES
+MBAGA1UECgwJbG9jYWxob3N0MQswCQYDVQQLDAJDTjESMBAGA1UEAwwJZnRwc2Vy
+dmVyMB4XDTIxMTEyMzE0NTI1OVoXDTMxMTEyMTE0NTI1OVowYjELMAkGA1UEBhMC
+SU4xDjAMBgNVBAgMBURlbGhpMQ4wDAYDVQQHDAVEZWxoaTESMBAGA1UECgwJbG9j
+YWxob3N0MQswCQYDVQQLDAJDTjESMBAGA1UEAwwJZnRwc2VydmVyMFkwEwYHKoZI
+zj0CAQYIKoZIzj0DAQcDQgAEAxgzx96lQzbvC+S2lNkGzFORalrFJ0jC5GWcKoSO
+yPOnaCRIlUwT2lZ9IMDkicWeHpAlPLqm0UxSgPFhEtLTl6NTMFEwHQYDVR0OBBYE
+FPmkknrazmMylpZOCXZkaNjOfad9MB8GA1UdIwQYMBaAFPmkknrazmMylpZOCXZk
+aNjOfad9MA8GA1UdEwEB/wQFMAMBAf8wCgYIKoZIzj0EAwIDSAAwRQIgRUxLR7F2
+hVC3+o33XbesY2K65aDExNKLJIEG/q3zeBMCIQCa5k9ijdiKSJEc5YyQGt+Nx4V/
+VI3LodE91HHz1aCHig==
+-----END CERTIFICATE-----
+`
 func main(){
-	server, err := net.Listen("tcp6", ":"+PORT)
+
+	cer, err := tls.X509KeyPair([]byte(serverCert), []byte(serverKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := &tls.Config{Certificates: []tls.Certificate{cer}}
+
+
+	server, err := tls.Listen("tcp", ":"+PORT, config)
 
 	defer server.Close()
 
@@ -34,11 +67,14 @@ func main(){
 	}
 	log.Println("TCP server is UP @ localhost:", PORT)
 
+
 	for{
 		connection, err := server.Accept()
+
 		if err != nil{
 			log.Println("Client Connection failed")
 		}
+
 		go HandleServer(connection)
 	}
 }
@@ -66,7 +102,6 @@ func AuthenticateClient(conn net.Conn){
 		passwd := reader.Text()
 
 		for _, cred := range *CREDS{
-
 			if cred.Username == uname && cred.Password == passwd{
 				validated=true
 				log.Println("Client Validated")
